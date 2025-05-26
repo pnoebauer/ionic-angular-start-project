e064b06477b2acad037a61f072d7ea05d54f85aa
Philip Noebauer <philip.noebauer@86400.com.au>
2025-05-26 11:31:56 +1000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ionic-angular-start-project',
+  webDir: 'www'
+};
+
+export default config;
